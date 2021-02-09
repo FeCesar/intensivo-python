54ed6b5f4adb558525e95858ed266f1b6de6a577
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-08 21:02:12 -0300

    Ordenando Colunas

--- automacao-processos/main.py
+++ automacao-processos/main.py
@@ -3,4 +3,7 @@ import pandas as pd
 databaseName = "./automacao-processos/database/vendas.xlsx"
 tableSell = pd.read_excel(databaseName)
 
-print(tableSell)
+revenuesColumns = tableSell[["ID Loja", "Valor Final"]].groupby("ID Loja").sum()
+revenuesColumns = revenuesColumns.sort_values(by="Valor Final", ascending=False)
+
+print(revenuesColumns)
