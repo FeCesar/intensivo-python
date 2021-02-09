74ec6075d21343f23ddb1eea6e8d3e98bd3137ac
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-08 21:16:15 -0300

    Analisando o ticket médio

--- automacao-processos/main.py
+++ automacao-processos/main.py
@@ -6,4 +6,10 @@ tableSell = pd.read_excel(databaseName)
 revenuesColumns = tableSell[["ID Loja", "Valor Final"]].groupby("ID Loja").sum()
 revenuesColumns = revenuesColumns.sort_values(by="Valor Final", ascending=False)
 
-print(revenuesColumns)
+amountColumns = tableSell[["ID Loja", "Quantidade"]].groupby("ID Loja").sum()
+amountColumns = amountColumns.sort_values(by="Quantidade", ascending=False)
+
+ticketMiddle = revenuesColumns["Valor Final"] / amountColumns["Quantidade"]
+ticketMiddle = ticketMiddle.to_frame()
+print(ticketMiddle)
+
