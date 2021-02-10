a2e3b176a0da8a6df9d958ebcad5c1c0a41cf1d0
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-10 15:08:41 -0300

    Análisando os clientes e cancelados por porcentagem

--- analise-dados/main.py
+++ analise-dados/main.py
@@ -7,9 +7,14 @@ table = table.drop("CLIENTNUM", axis=1) #axis = 0: Linha / axis = 1: Coluna
 
 # DATA PROCESSING
 table = table.dropna() # Delete null rows
-infoTable = table.info() # Overview of table
+# infoTable = table.info() # Overview of table
 overview = table.describe() # Describe all of the base
 
-print(overview)
-
+# ANALYZE
+caregoryTable = table["Categoria"].value_counts(normalize=True).mul(100).round(1).astype(str) + '%' 
+    # normalize=True: Mostra os valores em procentagem
+    # mul(100): Multiplica por 100
+    # round(1): Arredonda em uma casa decimal
+    # astyoe(str): Converte em string
+    # +: Concatena com '%'
 
