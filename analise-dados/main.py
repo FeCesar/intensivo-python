e480f5dca3b8acee3ec9043821073aadcbc1a155
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-10 14:52:53 -0300

    Processando os Dados

--- analise-dados/main.py
+++ analise-dados/main.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
+# IMPORT TABLE
+# DROPING USELESS COLUMN
 table = pd.read_csv(r'./analise-dados/database/clientes.csv', encoding="latin1")
-# Sempre tratar os dados
-# Excluir ou Corrigir Células vazias
-# Excluir colunas inúteis
+table = table.drop("CLIENTNUM", axis=1) #axis = 0: Linha / axis = 1: Coluna
 
-print(table)
+# DATA PROCESSING
+table = table.dropna() # Delete null rows
+infoTable = table.info() # Overview of table
+overview = table.describe() # Describe all of the base
+
+print(overview)
+
+
