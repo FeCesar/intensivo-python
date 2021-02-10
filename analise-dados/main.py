40bfeb32d5e63f53707803f33d44e699bc7c62a7
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-10 14:13:36 -0300

    Lendo a tabela em utf8

--- analise-dados/main.py
+++ analise-dados/main.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-table = pd.read_csv('./analise-dados/database/clientes.csv')
+table = pd.read_csv(r'./analise-dados/database/clientes.csv', encoding="latin1")
+# Sempre tratar os dados
+# Excluir ou Corrigir Células vazias
+# Excluir colunas inúteis
+
 print(table)
