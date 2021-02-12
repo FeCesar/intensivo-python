0a4ae4e5641c16fe90a5094b1e23ba5a9e7784d5
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-12 02:49:44 -0300

    Acessando site e colocando informações

--- web-scraping/main.py
+++ web-scraping/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from selenium import webdriver
 
-table = pd.read_excel("web-scraping/database/Clientes Pagamento.xlsx")
+table = pd.read_excel("web-scraping/database/Clientes Pagamento.xlsx", dtype={"Cliente": object})
 table = table[["Nome", "Valor Pago", "Valor Total Devido", "Email"]]
 
 devedores = table["Valor Total Devido"] - table["Valor Pago"]
@@ -12,4 +13,22 @@ for x in devedores:
 table["Deve"] = valores
 table = table[["Nome", "Deve", "Email"]]
 table = table.loc[table["Deve"] != 0]
+linhas = len(table)
+table = table["Nome"]
+
+nomes = []
+for name in table:
+    nome = name + ","
+    nomes.append(nome)
+
+# .loc[Linha, Coluna]
+
+navegador = webdriver.Chrome()
+navegador.get("http://www.sorteandoja.com.br/")
+navegador.find_element_by_id("fechar-modal-automatico").click()
+navegador.find_element_by_id("sj_quantidade3").clear()
+navegador.find_element_by_id("sj_quantidade3").send_keys(linhas)
+navegador.find_element_by_id("sj_itens").send_keys(nomes)
+navegador.find_element_by_id("sj-bt-sortear3").click()
+
 
